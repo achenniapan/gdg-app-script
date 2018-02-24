af1daf5dbbb391ec6a1f0ababd24a297581ebe65
Arunkumar Chenniapan <a.chenniapan@gmail.com>
2018-02-24 12:40:57 +0530

    sheet integration

--- Code.js
+++ Code.js
@@ -5,12 +5,17 @@ var sheetID = "1FkJM7ifjl6_7yAD8mbTZ7TpNZAXzjcxWrabFzZNutAU";
 var sheet =   SpreadsheetApp.openById(sheetID).getSheetByName("Sheet1");
 var db = PropertiesService.getScriptProperties();
 
-
+function runme(){
+ getCommits('achenniapan/gdg-app-script'); 
+}
 function getCommits(repo) {
   var api_url = base_url + repo + commit_url; 
   var results = fetch(api_url);
-  for each( var result in results) {	
-     //TODO
+  for each(var result in results){
+    console.log(result);
+    var commitname=result.commit.author.name;
+    var commitmsg=result.commit.message;
+    sheet.appendRow([commitname,commitmsg]);
   }
 }
 
